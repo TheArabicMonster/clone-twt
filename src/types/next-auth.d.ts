129c72fbf943863687d315368639c7a12c3bf08c
TheArabicMonster <neetamk@icloud.com>
2026-02-11 09:19:54 +0000

    refactor: mise à jour des types NextAuth
    - Ajouté 'pseudo: string' aux interfaces Session, User et JWT

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,11 +5,13 @@ declare module "next-auth" {
     user: {
       id: string;
       username: string;
+      pseudo: string;
     } & DefaultSession["user"];
   }
 
   interface User {
     username: string;
+    pseudo: string;
   }
 }
 
@@ -17,5 +19,6 @@ declare module "next-auth/jwt" {
   interface JWT {
     id: string;
     username: string;
+    pseudo: string;
   }
 }
