c530ec63ea08f69b7b0caa5a121c26cccab19052
Mateen Khalil <pq40rdc@eduvaud.ch>
2026-02-13 12:02:15 +0100

    refactor: adapter la page d'accueil pour la nouvelle structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,20 @@
 import { auth } from "@/lib/auth";
 import { HomeContent } from "@/components/HomeContent";
-import { AppShell } from "@/components/AppShell";
 
 export default async function Home() {
   const session = await auth();
 
   return (
-    <AppShell username={session?.user?.username}>
-      <HomeContent
-        user={
-          session?.user
-            ? {
-                pseudo: session.user.pseudo,
-                email: session.user.email,
-                username: session.user.username,
-              }
-            : null
-        }
-      />
-    </AppShell>
+    <HomeContent
+      user={
+        session?.user
+          ? {
+              pseudo: session.user.pseudo,
+              email: session.user.email,
+              username: session.user.username,
+            }
+          : null
+      }
+    />
   );
 }
