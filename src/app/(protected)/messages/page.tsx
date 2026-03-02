a1e9b0395ba2ca3abde2eb4131afb184ec9e399d
TheArabicRank1 <you@example.com>
2026-03-02 14:40:31 +0100

    feat: créer la page Messages

---
+++ src/app/(protected)/messages/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useState } from "react";
+
+export default function Messages() {
+
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">Messages</h1>
+            <p className="text-lg text-default-500">page de messages.</p>
+        </div>
+    );
+}
