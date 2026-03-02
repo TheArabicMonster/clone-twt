6ae6dd615777536366dcaed889f3554efc95cb9f
TheArabicRank1 <you@example.com>
2026-03-02 10:47:59 +0100

    refactor: déplacer le provider Credentials de authConfig vers auth.ts

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,49 +1,9 @@
 import type { NextAuthConfig } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import { prisma } from "@/lib/prisma";
+
 
 export const authConfig: NextAuthConfig = {
   providers: [
-    Credentials({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Email et mot de passe requis");
-        }
-
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email as string,
-          },
-        });
-
-        if (!user || !user.hashedPassword) {
-          throw new Error("Identifiants invalides");
-        }
-
-        const isPasswordValid = await bcrypt.compare(
-          credentials.password as string,
-          user.hashedPassword,
-        );
-
-        if (!isPasswordValid) {
-          throw new Error("Identifiants invalides");
-        }
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          image: user.image,
-          username: user.username,
-        };
-      },
-    }),
+    
   ],
   pages: {
     signIn: "/login",
@@ -57,6 +17,7 @@ export const authConfig: NextAuthConfig = {
       if (user) {
         token.id = user.id;
         token.username = user.username as string;
+        token.pseudo = user.pseudo as string;
       }
       return token;
     },
@@ -64,21 +25,27 @@ export const authConfig: NextAuthConfig = {
       if (session.user) {
         session.user.id = token.id as string;
         session.user.username = token.username as string;
+        session.user.pseudo = token.pseudo as string;
       }
       return session;
     },
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnAuth =
-        nextUrl.pathname.startsWith("/login") ||
-        nextUrl.pathname.startsWith("/signup");
+      console.log(auth?.user);
+        const isLoggedIn = !!auth?.user;
+        const isOnAuth =
+          nextUrl.pathname.startsWith("/login") ||
+          nextUrl.pathname.startsWith("/signup");
 
-      if (isOnAuth) {
-        if (isLoggedIn) return Response.redirect(new URL("/", nextUrl));
-        return true;
-      }
+        const isOnLandingPage = nextUrl.pathname==="/";
+        if(isOnLandingPage&&isLoggedIn) {
+          return Response.redirect(new URL("/profil", nextUrl));
+        }
+        if (isOnAuth) {
+          if (isLoggedIn) return Response.redirect(new URL("/profil", nextUrl));
+          return true;
+        }
 
-      return true; // Permet l'accès à toutes les autres routes
-    },
+        return true; // Permet l'accès à toutes les autres routes
+      },
   },
 };
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,51 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth.config";
+import bcrypt from "bcryptjs";
+import { prisma } from "@/lib/prisma";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
+    providers: [
+    Credentials({
+        name: "credentials",
+        credentials: {
+          email: { label: "Email", type: "email" },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize(credentials) {
+          if (!credentials?.email || !credentials?.password) {
+            throw new Error("Email et mot de passe requis");
+          }
+
+          const user = await prisma.user.findUnique({
+            where: {
+              email: credentials.email as string,
+            },
+          });
+
+          if (!user || !user.hashedPassword) {
+            throw new Error("Identifiants invalides");
+          }
+
+          const isPasswordValid = await bcrypt.compare(
+            credentials.password as string,
+            user.hashedPassword,
+          );
+
+          if (!isPasswordValid) {
+            throw new Error("Identifiants invalides");
+          }
+
+          return {
+            id: user.id,
+            email: user.email,
+            name: user.pseudo,
+            image: user.image,
+            username: user.username,
+            pseudo: user.pseudo,
+          };
+        },
+      }),
+  ],
 });
