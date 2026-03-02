22187d4756ac058c035a5a9e2d2b871168ca9d63
TheArabicRank1 <you@example.com>
2026-03-02 14:40:25 +0100

    feat: créer la page Timeline (timline)

---
+++ src/app/(protected)/timline/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Timline() {
+
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">Timline</h1>
+            <p className="text-lg text-default-500">page de timline.</p>
+        </div>
+    );
+}
