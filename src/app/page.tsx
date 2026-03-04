9111514a8da9496154e714b9b313df2fbb137644
TheArabicRank1 <you@example.com>
2026-03-04 11:47:32 +0100

    refactor: rediriger les utilisateurs connectés vers leur profil personnel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await auth();
   if (session) {
-    redirect("/profil");
+    redirect(`/profil/${session.user.username}`);
   }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-8">
