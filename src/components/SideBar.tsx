f9c79109a4deba5d2a968bb0587ea2544096028f
TheArabicRank1 <you@example.com>
2026-03-04 11:47:44 +0100

    feat: utiliser useSession dans le SideBar pour les liens généralisés

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,11 +3,12 @@ import { User, House, Mail } from "lucide-react";
 import { Button } from "@heroui/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export default function SideBar() {
     const pathname = usePathname();
-
     const isActive = (href: string) => pathname === href;
+    const session = useSession().data;
 
     return (
         <div className="ml-2 mt-2 mb-2 mr-4 w-14 h-[calc(100vh-1rem)] bg-gray-1000 p-2 bg-gradient-to-b from-gray-900 to-gray-800 rounded-lg outline-2 outline-offset-2 flex flex-col">
@@ -17,8 +18,8 @@ export default function SideBar() {
                     className={`transition-all duration-300 rounded-md ${isActive("/timline") ? "bg-white/20" : "bg-transparent"}`}>
                     <House className="text-white w-auto h-auto" size={40} strokeWidth={1.4} />
                 </Button>                
-                <Button as={Link} href="/profil" isIconOnly variant="light"
-                    className={`transition-all duration-300 rounded-md ${isActive("/profil") ? "bg-white/20" : "bg-transparent"}`}>
+                <Button as={Link} href={`/profil/${session?.user.username}`} isIconOnly variant="light"
+                    className={`transition-all duration-300 rounded-md ${isActive(`/profil/${session?.user.username}`) ? "bg-white/20" : "bg-transparent"}`}>
                     <User className="text-white w-auto h-auto" size={40} strokeWidth={1.4} />
                 </Button>
                 <Button as={Link} href="/messages" isIconOnly variant="light"
