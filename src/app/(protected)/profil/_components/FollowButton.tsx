b02951fb39b5b425782217e9a77875a56e130f88
TheArabicRank1 <you@example.com>
2026-03-04 11:48:01 +0100

    feat: créer le composant FollowButton pour gérer les abonnements

---
+++ src/app/(protected)/profil/_components/FollowButton.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { Button } from "@heroui/react";
+export default function FollowButton() {
+    return (
+        <Button color="primary" variant="solid" className="w-full">
+            Follow
+        </Button>
+    );
+}
