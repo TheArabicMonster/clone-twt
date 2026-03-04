bd22aeaa00bead6fa8beb4300ec81afde33b65aa
TheArabicRank1 <you@example.com>
2026-03-04 11:47:25 +0100

    feat: ajouter SessionProvider pour la gestion des sessions client

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,16 @@
 
 import { HeroUIProvider } from "@heroui/react";
 import { useRouter } from "next/navigation";
+import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
   return (
-    <HeroUIProvider navigate={router.push}>
-      {children}
-    </HeroUIProvider>
+    <SessionProvider>
+      <HeroUIProvider navigate={router.push}>
+        {children}
+      </HeroUIProvider>
+    </SessionProvider>
   );
 }
